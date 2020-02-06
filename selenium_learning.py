f3f100339c7b5e044c6e11aa7c20074e0bd3ddd3
KOGLinese <1076452761@qq.com>
2020-02-06 12:28:37 +0800

    selenium

--- selenium_learning.py
+++ selenium_learning.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
-
+# 创建了一个 Chrome 驱动
 driver = webdriver.Chrome()
+# 使用 get 方法打开百度
 driver.get("https://www.baidu.com")
 
+# 获取到输入框之后我们就往里面写入我们要搜索的内容
 input = driver.find_element_by_css_selector('#kw')
 input.send_keys("linese")
-
+# 获取到搜索这个按钮 然后点击
 button = driver.find_element_by_css_selector('#su')
 button.click()
+
+# 爬完之后要关闭
+# driver.close()
